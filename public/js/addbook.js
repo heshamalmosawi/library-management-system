e62977f773e2f83457c6badbb084065ee35f5ad2
heshamalmosawi <hishamalmosawii@gmail.com>
2024-05-10 15:29:22 +0300

    Front end and routing for adding a book

---
+++ public/js/addbook.js
@@ -0,0 +1,118 @@
+var authors = 1; //number of authors starts minimally at 1
+
+// This function validates the ISBN input dynamically
+function validate(isbn) {
+    var isbnhtml = document.getElementById("isbnvalidate");
+    var autofillbtn = document.getElementById("autofillBtn");
+
+    // if ISBN is empty, Not a number, or not 13 digits, give validation message and block autofill button.
+    isbn = isbn.trim() // trimming beginning and ending spaces.
+    if (isbn == "") {
+        isbnhtml.innerHTML = "Please enter ISBN!";
+        autofillbtn.disabled = true;
+        return;
+    }
+    if (isNaN(isbn)) {
+        isbnhtml.innerHTML = "Invalid format! Only include digits.";
+        autofillbtn.disabled = true;
+        return;
+    }
+    if (isbn.length != 13) {
+        isbnhtml.innerHTML = "ISBN should be 13 Digits!";
+        autofillbtn.disabled = true;
+        return;
+    } else { // if reached this point, then format is valid.
+        isbnhtml.innerHTML = "";
+        autofillbtn.disabled = false;
+    }
+}
+
+// This function calls google boooks API primarly and open library API secondary in case of any errors, then fills data dynamically.
+async function callApi() {
+    var isbn = document.getElementById("ISBN").value;
+    validate(isbn); // revalidating just in case!
+
+    try {
+        // API request to fetch book details
+        const response = await fetch(
+            "https://www.googleapis.com/books/v1/volumes?q=ISBN:" + isbn
+        );
+        // Parse response JSON
+        const data = await response.json();
+        // Extract book information from the api request
+        const bookInfo = await data.items[0].volumeInfo;
+
+        // filling book form fields
+        document.getElementsByName("Title")[0].value = bookInfo.title;
+        numAuthors = bookInfo.authors.length;
+        document.getElementsByName("Author[]")[0].value = bookInfo.authors[0];
+
+        fillAuthorFields(numAuthors, bookInfo);
+
+        document.getElementsByName("Category")[0].value =
+            bookInfo.categories[0];
+        document.getElementsByName("Description")[0].value =
+            bookInfo.description;
+        var pic = bookInfo.imageLinks.thumbnail;
+        if (pic != "") {
+            document.getElementsByName("BookCover")[0].value = pic;
+        } else {
+            // secondAPI()
+        }
+    } catch (error) {
+        console.error("Error fetching book details:", error);
+    }
+}
+
+// This function fills in multiple authors as needed, and removing extra unnecessary fields
+function fillAuthorFields(numAuthors, bookInfo) {
+    for (let i = 1; i < numAuthors; i++) {
+        if (!document.getElementsByName("Author[]")[i]) {
+            addAuthor();
+        }
+        document.getElementsByName("Author[]")[i].value = bookInfo.authors[i];
+    }
+
+    numOfAuthorFields = document.getElementsByName("Author[]").length;
+    for (let i = numOfAuthorFields - 1; i >= numAuthors; i--) {
+        removeAuthor();
+    }
+}
+
+// This function adds author text fields as needed on call.
+function addAuthor() {
+    ++authors;
+    // Get the container element
+    const container = document.getElementById("AuthorOption");
+
+    // Create a new input element
+    const inputField = document.createElement("input");
+
+    // Set the input field attributes
+    inputField.type = "text";
+    inputField.name = "Author[]";
+    inputField.placeholder = "Author " + authors;
+
+    // Add the input field to the container
+    container.appendChild(inputField);
+}
+
+// This function removes the last input text field
+function removeAuthor() {
+    // Get the container element
+    const container = document.getElementById("AuthorOption");
+
+    // Get all input fields from the container
+    const inputFields = container.getElementsByTagName("input");
+
+    // Check if there is more than one input field
+    if (inputFields.length > 1) {
+        // Remove the last input field
+        container.removeChild(inputFields[inputFields.length - 1]);
+        --authors;
+    }
+}
+
+// async function secondAPI(){
+
+// }
--- resources/views/addbook.blade.php
+++ resources/views/addbook.blade.php
@@ -6,131 +6,93 @@
     <title>Add book</title>
     {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> <!-- Assuming your CSS file is in a folder named 'css' --> --}}
 </head>
+<style>
+    .errors {
+        color: red;
+    }
+</style>
 <div>
     <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-    {{-- <input type="image" border=50px src=hello.jpg> --}}
-    <form action="POST">
+    <form method="POST" action="#">
+        @csrf
         <label for="ISBN">ISBN</label>
-            <input type="text" name="ISBN" id=ISBN onkeyup="validate(this.value)" placeholder=123456789012 required>
-            <p id=isbnvalidate></p>
+            <input type="text" name="ISBN" id=ISBN onkeyup="validate(this.value)" placeholder=ISBN-13 required>
+            <p id=isbnvalidate> </p>
+            @error('ISBN')
+            <p class=errors> {{ $message }}</p>
+            @enderror
+        <br>
         <label for="Title">Title</label>
             <input type="text" name="Title" required>
-            <label for="Author">Author</label>
-        <div id="AuthorOption">
-        <input type="text" placeholder="Author" name="Author[]" required>
-        </div>
-        
-
-
+        <br>
+        <label for="Author">Author</label>
+            <div id="AuthorOption">
+                <input type="text" placeholder="Author" name="Author[]" required>
+            </div>
+            @error('Author')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+<br>
         <label for="Category">Category</label>
             <input type="text" name="Category" required>
+            @error('Category')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+<br>
         <label for="Description">Description</label>
-            <input type="text" name="Description" required>
+            <textarea name="Description" required></textarea>
+            @error('Description')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+<br>
+
         <label for="BookCover">Book Cover URL</label>
             <input type="text" name="BookCover" required>
+            @error('BookCover')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+    <br>    
+        
 
-            <br>
+        <label for="publisher">Publisher</label>
+            <input type="text" name="publisher" required>
+            @error('publisher')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+        <br>
+        <label for="publish_year">Publish Year</label>
+            <input type="number" name="publish_year" required>
+            @error('publish_year')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+
+        <br>
+        <label for="total_copies">Total Copies</label>
+            <input type="number" name="total_copies" required>
+            @error('total_copies')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+        
+        <br>
+        <label for="location">Branch Location</label>
+            <input type="text" name="location" required>
+            @error('location')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+        <br>
+        <label for="num_of_pages">Number of Pages</label>
+            <input type="number" name="num_of_pages" required>
+            @error('num_of_pages')
+                <p class=errors> {{ $message }}</p>
+            @enderror
+
+        
+        <br>
         <button type="button" onclick="callApi()" id=autofillBtn disabled> Autofill </button>
-        <button onclick="addAuthor()">Add Author</button>
-    <button onclick="removeAuthor()">Remove Author</button>
+        <button type=button onclick="addAuthor()">Add Author</button>
+        <button type=button onclick="removeAuthor()">Remove Author</button>
 
         <button type="submit">Add book</button>
     </form>
-    <script>
-         var authors=1;
-        function validate(isbn){
-            var isbnhtml = document.getElementById("isbnvalidate");
-            var autofillbtn = document.getElementById("autofillBtn");
-            if (isbn.trim() == "") {
-                isbnhtml.innerHTML = "Please enter ISBN!"
-                // autofillbtn.disabled = true;
-                return
-            }
-            if (isNaN(isbn)){
-                isbnhtml.innerHTML = "Invalid format! Only include digits."
-                autofillbtn.disabled = true;
-                return;
-            }
-            if (isbn.length != 13){
-                isbnhtml.innerHTML = "ISBN should be 13 Digits!"
-                autofillbtn.disabled = true;
-                return;
-            } else {
-                isbnhtml.innerHTML = ""
-                autofillbtn.disabled = false;
-            }
-        }
-
-        async function callApi(){
-            var isbn = document.getElementById("ISBN").value;
-            validate(isbn)
-
-            try {
-                // API request to fetch book details
-                const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=ISBN:' + isbn);
-                // Parse response JSON
-                const data = await response.json();
-                // Extract book information from the api request
-                const bookInfo = await data.items[0].volumeInfo;
-
-                // fill book form field
-                document.getElementsByName("Title")[0].value = bookInfo.title;
-                numAuthors = bookInfo.authors.length;
-                document.getElementsByName("Author[]")[0].value = bookInfo.authors[0];
-                for (let i = 1 ; i < numAuthors; i++)
-                {
-                    addAuthor();
-                    document.getElementsByName("Author[]")[i].value = bookInfo.authors[i];
-                }
-                
-                document.getElementsByName("Category")[0].value = bookInfo.categories[0];
-                document.getElementsByName("Description")[0].value = bookInfo.description;
-                var pic = bookInfo.imageLinks.thumbnail;
-                if (pic != ""){
-                    document.getElementsByName("BookCover")[0].value = pic;  
-                } else {
-                    // secondAPI()
-                }
-  
-            } catch (error) {
-                console.error('Error fetching book details:', error);
-            }
-        }
-        function addAuthor() {
-            ++authors
-            // Get the container element
-            const container = document.getElementById('AuthorOption');
-            
-            // Create a new input element
-            const inputField = document.createElement('input');
-            
-            // Set the input field attributes
-            inputField.type = 'text';
-            inputField.name = 'Author[]';
-            inputField.placeholder="Author "+authors; 
-            
-            
-            // Add the input field to the container
-            container.appendChild(inputField);
-        }
-
-        // Function to remove the last input text field
-        function removeAuthor() {
-            // Get the container element
-            const container = document.getElementById('AuthorOption');
-            
-            // Get all input fields from the container
-            const inputFields = container.getElementsByTagName('input');
-            
-            // Check if there is more than one input field
-            if (inputFields.length > 1) {
-                // Remove the last input field
-                container.removeChild(inputFields[inputFields.length - 1]);
-                --authors
-            }
-        }
-        // async function secondAPI(){
-
-        // }
-    </script>
 </div>
+<script src="{{ asset('js/addbook.js') }}"></script>
