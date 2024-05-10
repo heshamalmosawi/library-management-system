7f461d0574053b2b3a70503885b77757bf37e432
heshamalmosawi <hishamalmosawii@gmail.com>
2024-05-10 15:28:19 +0300

    finished addbook controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,33 +13,38 @@ class BookController extends Controller
     }
 
     public function addBook(Request $request){
+        // dd($request->input('Author'));
+        
         $request->validate([
           'ISBN' => 'required|digits:13',
           'Title' => 'required|string|max:50',
-        //   'authors' =>'required'
-           'BookCover' => 'required|string|max:255',
-           'publisher' => 'required|string|max:50',
-           'publish_year' => 'required|integer|min:1000|max:' . date('Y'), // Assuming publish year is between 1000 and current year
-           'Description' => 'required|string', // can have lots of characters!
-           'available_copies' => 'required|integer|min:0|max:999', 
-           'total_copies' => 'required|integer|min:0|max:999', 
-           'num_of_pages' => 'required|integer|min:1|max:9999', 
-           'is_archived' => 'required|boolean',
+          'Author.*' =>'required',
+          'Category' => 'required',
+          'BookCover' => 'required|string|max:255',
+          'publisher' => 'required|string|max:50',
+          'publish_year' => 'required|integer|min:1000|max:' . date('Y'), // Assuming publish year is between 1000 and current year
+          'Description' => 'required|string', // can have lots of characters!
+          'total_copies' => 'required|integer|min:0|max:999', 
+          'num_of_pages' => 'required|integer|min:1|max:9999', 
         ]);
-    
+
         $book = new Book();
+        // dd($request->all());
         $book->ISBN = $request->ISBN;
         $book->title = $request->Title;
-        $book->author = $request->Author;
+
+        $authors = implode(', ', $request->input('Author.*'));
+        $book->author = $authors;
         $book->bookcover_url = $request->BookCover;
         $book->publisher = $request->publisher;
-        $book->publish_year = $request->publish_year;
+        $book->publish_date = $request->publish_year;
         $book->abstract = $request->Description;
-        $book->available_copies = $request->available_copies;
+        $book->available_copies = $request->total_copies; // initially, available copies = total copies
         $book->total_copies = $request->total_copies;
-        $book->num_of_pages = $request->num_of_pages;
-        $book->is_archived = $request->is_archived;
-        
+        $book->location = $request->location;
+        $book->numOfPages = $request->num_of_pages;
+        $book->is_archived = false;
+        // dd($request->all());
         $book->save();
         return redirect('/')->with('success', 'Add book successful!');
     }
