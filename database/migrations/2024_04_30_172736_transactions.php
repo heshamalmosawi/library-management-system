3b16c31cb6d8c54ba7664cc0e6e09e7b8c70a5a0
heshamalmosawi <hishamalmosawii@gmail.com>
2024-05-16 02:50:38 +0300

    fix database typo

--- database/migrations/2024_04_30_172736_transactions.php
+++ database/migrations/2024_04_30_172736_transactions.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreign('book_id')->references('id')->on('books');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->binary('transaction_type')->comment('0: Borrow, 1: Return');
+            $table->binary('transaction_type')->comment('0: Borrow, 1: Reserve');
             $table->date('transaction_date');
             $table->date('due_date');
             $table->boolean('period_extended');
