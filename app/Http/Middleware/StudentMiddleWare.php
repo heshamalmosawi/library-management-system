7766c16aaa6d5c565c3e921bf38b4a1930645401
sayhusain <hishamooo123@gmail.com>
2024-05-21 19:43:34 +0300

    halfway middleware

---
+++ app/Http/Middleware/StudentMiddleWare.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class StudentMiddleWare
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        dd('heym');
+        Log::info('User is authenticated: ' . Auth::user()->name);
+
+        if (Auth::check()) {
+            Log::info('User is authenticated: ' . Auth::user()->name);
+            // Check if the authenticated user is an admin
+            if (Auth::user()) {
+                // If user is admin, allow access to the requested route
+                return $next($request);
+            }
+        }
+        abort(403);
+    }
+}
