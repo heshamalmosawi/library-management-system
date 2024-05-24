4fbe72ec3aaa404aa813f152bb6aa14f81979e27
heshamalmosawi <hishamalmosawii@gmail.com>
2024-05-24 03:00:56 +0300

    minor change

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,14 +17,17 @@
     <a class="{{ request()->is('/') ? 'active' : '' }}" href="/">HOME</a>
     <a class="{{ request()->is('books') ? 'active' : '' }}" href="/books">BOOKS</a>
     <a class="{{ request()->is('category') ? 'active' : '' }}" href="/category">CATEGORIES</a>
-    <a class="{{ request()->is('reserve-book') ? 'active' : '' }}" href="#about">RESERVE BOOK</a>
-    {{-- @if(request()) --}}
-    <a class="{{ request()->is('login') ? 'active' : '' }}" href="/login">LOG IN</a>
+    <a class="{{ request()->is('borrow') ? 'active' : '' }}" href="/borrow">BORROW BOOK</a>
+    <a class="{{ request()->is('reserve') ? 'active' : '' }}" href="/reserve">RESERVE BOOK</a>
+    @if(session('userType') == 'staff')
+        <a class="{{ request()->is('returnbook') ? 'active' : '' }}" href="/returnbook">RETURN BOOK</a>
+    @endif
     @auth
-    <a href=/logout>LOG OUT</a>
+        <a class="{{ request()->is('profile') ? 'active' : '' }}" href="/profile">EDIT PROFILE</a>
+        <a href=/logout>LOG OUT</a>
     @else
+        <a class="{{ request()->is('login') ? 'active' : '' }}" href="/login">LOG IN</a>
         <a class="{{ request()->is('register') ? 'active' : '' }}" href="/register">REGISTER</a>
     @endauth
-
 
   </div>
